365d715695f5f3adafd8cefa65fe2cf2bfb5d3a0
msrojas <alez011@hotmail.com>
2016-01-20 15:36:48 -0600

    Primer upload

--- ejemplo1.c
+++ ejemplo1.c
@@ -25,91 +25,101 @@
 int main()
 {
     clock_t start = clock();
-
+    //We define the table: columns, rows
     struct Table * table = init_table(3,5);  //Definimos la tabla: columnas, filas
     if(table == NULL)
         exit(1);
 
-
+    //We add a column
     if(!AgregarColumna(table, "Name")) //Agregamos una columna
     {		
         ht_destroy(table);
         exit(1);
     }
-
+    //We add an element to the column: Name
     if(!AgregarElemento(table, "Name", "Alex")) //Agregamos un elemento a la columna: Name
     {
         ht_destroy(table);
         exit(1);
     }
-
+    //We add an element to the column: Name
     if(!AgregarElemento(table, "Name", "Saul")) //Agregamos un elemento a la columna: Name
     {
         ht_destroy(table);
         exit(1);
     }
-
+    //We add an element to the column: Name
     if(!AgregarElemento(table, "Name", "Gerardo Torres")) //Agregamos un elemento a la columna: Name
     {
         ht_destroy(table);
         exit(1);
     }
 
-
+    //We add another column
     if(!AgregarColumna(table, "Edad")) //Agregamos otra columna
     {		
         ht_destroy(table);
         exit(1);
     }
-
+    //We add an element to the column: Edad
     if(!AgregarElemento(table, "Edad", "13")) //Agregamos un elemento a la columna: Edad
     {
         ht_destroy(table);
         exit(1);
     }
-
+    //We add an element to the column: Edad
     if(!AgregarElemento(table, "Edad", "19")) //Agregamos un elemento a la columna: Edad
     {
         ht_destroy(table);
         exit(1);
-    }
-
+    } 
+    //We add an element to the column: Edad
     if(!AgregarElemento(table, "Edad", "24")) //Agregamos un elemento a la columna: Edad
     {
         ht_destroy(table);
         exit(1);
     }
-
+    
+    //We add one more column
     if(!AgregarColumna(table, "Direccion")) //Agregamos una columna mas
     {		
         ht_destroy(table);
         exit(1);
     }
-
+    //We add an element to the column: Direccion
     if(!AgregarElemento(table, "Direccion", "Col. Hidalgo")) //Agregamos un elemento a la columna: Direccion
     {
         ht_destroy(table);
         exit(1);
     }
-
+    //We add an element to the column: Direccion
     if(!AgregarElemento(table, "Direccion", "Insurgentes")) //Agregamos un elemento a la columna: Direccion
     {
         ht_destroy(table);
         exit(1);
     }
-
+    //We add an element to the column: Direccion
     if(!AgregarElemento(table, "Direccion", "Calle Morelos")) //Agregamos un elemento a la columna: Direccion
     {
         ht_destroy(table);
         exit(1);
     }
 
-
+    //We print the table
     PrintTable(table, MYSQL_STYLE); //Imprimimos la tabla pasandole el estilo de los margenes 
     //Por el momento solo hay dos estilos: MYSQL_STYLE y RECTANGULO
     PrintTable(table, RECTANGULO);
 
-    ht_destroy(table); //Destruimos la tabla
+    /* O puedes imprimir una columna en especifico */
+    /* Or you can print a specific column*/
+    printf("\nO puedes imprimir una columna en especifico\n");
+    if(!ListarElementos(table, "Edad", MYSQL_STYLE))
+    {
+        ht_destroy(table);
+        exit(1);
+    }
+
+    ht_destroy(table); //Destruimos la tabla/We destroy the table
 
     clock_t end = clock();
     double seconds = (double)(end - start) / CLOCKS_PER_SEC;
