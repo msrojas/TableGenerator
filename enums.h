fb4d269701acd8876175db3a9ae0001c0845c491
msrojas <alez011@hotmail.com>
2016-01-20 15:13:23 -0600

    Primer upload

---
+++ enums.h
@@ -0,0 +1,27 @@
+/*	
+    TableGenerator
+    Copyright (C) 2016  Alejandro Torres Hernandez
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    Email: alez011@hotmail.com
+*/  
+
+#ifndef ENUMS_H
+#define ENUMS_H
+
+enum {RECTANGULO=1,MYSQL_STYLE,PRIMERA_COLUMNA,OTRA_COLUMNA,TOP,BOTTOM,TOP_BOTTOM};
+
+#endif
